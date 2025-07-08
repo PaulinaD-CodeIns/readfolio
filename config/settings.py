4b231e7d84da39c75838593fd5cfcdd61565da43
Paulina Dolder <paulid4628@gmail.com>
2025-07-08 11:57:10 +0100

    Fixed error in Allowed Hosts

--- config/settings.py
+++ config/settings.py
@@ -28,10 +28,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG') == 'True'
+DEBUG = os.getenv('DEBUG') == 'False'
 
 
-ALLOWED_HOSTS = ['readfolio.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
