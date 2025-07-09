080bf448973bd5f5bed9f9e43b4f8cadf4b6b2df
Paulina Dolder <paulid4628@gmail.com>
2025-07-09 11:37:51 +0100

    Added home blog-style page with reviews on a loop and pagination

--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
+from library.models import Review
 
 
+# Home page blog of reviews
 def home(request):
-    return render(request, 'pages/home.html')
+    public_reviews = Review.objects.filter(is_public=True).select_related('book').order_by('?')[:5]
+    return render(request, 'pages/home.html', {'public_reviews': public_reviews})
 
+
+
+
+
+# About us page
 def about(request):
     return render(request, 'pages/about.html')
